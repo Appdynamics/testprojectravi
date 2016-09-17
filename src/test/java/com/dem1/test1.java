759b92396926d33e0ceb6a62551bfe428a292101
Ravi <ravi.pujar@tarams.com>
2016-09-17 18:19:15 +0530

    DDD

---
+++ src/test/java/com/dem1/test1.java
@@ -0,0 +1,16 @@
+package com.dem1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class test1
+{
+	@Test
+	public void login()
+	{
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.google.com");
+	}
+
+}
